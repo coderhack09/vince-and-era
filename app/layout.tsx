6519f060db39ee9a9e092c476ce3e935909d049b
Rolando Valle <rolandovalle0428@gmail.com>
2026-04-02 17:49:33 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const eventImageUrl = `${canonicalUrl}${desktopHero}`
 // f_jpg forces JPEG so all OG scrapers (iMessage, Viber, Facebook, etc.) can display it.
 // The public-folder URL is kept only as a fallback in the images array below.
 const OG_IMAGE_CLOUDINARY =
-  "https://res.cloudinary.com/detbgvt1c/image/upload/f_jpg,q_auto,w_1200,h_630,c_fill/wedding-projects/vince-and-era-invites/Details/LinkPreview.jpg"
+  "https://res.cloudinary.com/detbgvt1c/image/upload/f_jpg,q_auto,w_1200,h_630,c_fill/wedding-projects/vince-and-era/Details/LinkPreview.jpg"
 const OG_IMAGE_FALLBACK = `${canonicalUrl}${desktopHero}`
 
 const coupleNames = `${siteConfig.couple.groomNickname} & ${siteConfig.couple.brideNickname}`
