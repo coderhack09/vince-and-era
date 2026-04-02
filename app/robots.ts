5b21ab3206c6cf982ed83671d893a34f311d3f32
Rolando Valle <rolandovalle0428@gmail.com>
2026-04-02 18:03:48 +0800

    update

--- app/robots.ts
+++ app/robots.ts
@@ -2,17 +2,10 @@ import type { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        // Explicitly allow Facebook's link-preview scraper so OG tags are readable
-        userAgent: "facebookexternalhit",
-        allow: "/",
-      },
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
-    sitemap: "https://vince-and-era.weddinginvitationrsvp.com/sitemap.xml",
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://vince-and-era-invites.weddinginvitationrsvp.com/sitemap.xml",
   }
 }
